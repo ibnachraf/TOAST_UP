53afb9446e06798fd9ba5c8daa4776becb662080
ibnachraf <ibnmrabet.achraf@gmail.com>
2017-06-09 00:44:00 +0200

    third commit

--- src/main/main1.java
+++ src/main/main1.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
+import java.util.Calendar;
 
 /**
  * Created by h-raf on 03/06/17.
@@ -27,14 +28,22 @@ public class main1 {
                 .and()
                 .build();
         JtwigTemplate template
-                = JtwigTemplate.classpathTemplate("index.html",conf);
+                = JtwigTemplate.classpathTemplate("./assets/fiche.twig.html",conf);
         JtwigModel model = JtwigModel.newModel()
-                .with("filiere","holaaaaaaaaaa tuuuuuu2");
+                .with("filiere", Calendar.getInstance().getTime());
         String output = template.render(model);
 
+        toPDF(output);
 
+
+    }
+
+    private static String toPDF(String output/*,Evaluation eval */){
+        String pathHtml = "./HTML/fiche_eval_HTML.html";
         try{
-            PrintWriter writer = new PrintWriter("index1.html", "UTF-8");
+            //String pathHtml = "./HTML/fiche_evaluation_HTML"+eval.getStage().getEtudiant().getIdentEtudiant()+"_"+eval.getStage().getEtudiant().getNomUsuel()+".html";
+
+            PrintWriter writer = new PrintWriter(pathHtml, "UTF-8");
             writer.write(output);
             writer.close();
         } catch (IOException e) {
@@ -42,13 +51,14 @@ public class main1 {
         }
 
 
-        String cmd = "wkhtmltopdf --enable-javascript --debug-javascript /home/h-raf/IdeaProjects/T/index1.html pdf";
+        String path = "./PDF/fiche_evaluation_.pdf";
+        String cmd = "wkhtmltopdf --enable-javascript --debug-javascript "+pathHtml+" "+path;
         String out = executeCommand(cmd);
-        String cmd2 = "rm indexo.html";
+        String cmd2 = "rm index1.html";
         executeCommand(cmd2);
-        System.out.println(out);
-        
+        System.out.println("exec: "+out);
 
+        return path;
 
     }
 
